cfaa4a7c1735e5f8e32add76dd887356084269f6
joenapper <joe.napper@hotmail.co.uk>
2021-10-20 16:22:30 +0100

    Created basic GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body {
+    font-family: "Arial", sans-serif;
+    background-color: red;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  p {
+    margin-bottom: 1rem;
+    
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
+export default GlobalStyles;
